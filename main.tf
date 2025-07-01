ad049cab4ee837fccceb1e3867b326394859e206
Curtis Hodak <curtish@spacelift.com>
2025-07-01 14:23:16 -0400

    add context

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "spacelift_worker_pool" "k8s-core" {
   csr         = filebase64("workerpooltest.csr")
   description = "test"
 }
+
+resource "spacelift_context" "prod-k8s-ie" {
+  description = "Configuration details for the compute cluster in 🇮🇪"
+  name        = "Production cluster (Ireland)"
+}

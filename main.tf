bb13bd5bcf49118a81eaeb5b4c81c581ec0b409e
Curtis Hodak <curtish@spacelift.com>
2025-07-01 14:08:41 -0400

    testing worker pool

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "spacelift_worker_pool" "k8s-core" {
   name        = "Main worker"
   csr         = filebase64("workerpooltest.csr")
-  description = "Used for all types of jobs"
+  description = "test"
 }

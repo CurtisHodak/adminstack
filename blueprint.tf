6c551142755cad34d59bd7ed9f49d13b524d09d2
Curtis Hodak <curtish@spacelift.com>
2025-07-07 16:30:05 -0400

    change to file location

--- blueprint.tf
+++ blueprint.tf
@@ -3,5 +3,5 @@ resource spacelift_blueprint "blueprint1" {
     space = "root"
     state = "PUBLISHED"
     description = "testing"
-    template = "blueprint1.tpl"
+    template = file("${path.module}/blueprint1.tpl")
 }

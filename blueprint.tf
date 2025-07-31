ad25b81e599840507ea46c39cde6555d3c939667
Curtis Hodak <curtish@spacelift.com>
2025-07-31 16:44:50 -0400

    fixing

--- blueprint.tf
+++
@@ -1,7 +0,0 @@
-resource spacelift_blueprint "blueprint1" {
-    name = "blueprint1"
-    space = "root"
-    state = "PUBLISHED"
-    description = "testing"
-    template = file("${path.module}/blueprint1.yaml")
-}

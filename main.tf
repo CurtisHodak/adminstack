d9045eb8226392e36f8409c21bf233d9075e0833
Curtis Hodak <curtish@spacelift.com>
2025-09-09 12:37:00 -0400

    space write

--- main.tf
+++ main.tf
@@ -83,5 +83,5 @@ resource "spacelift_aws_integration_attachment" "module_rw" {
 resource "spacelift_role" "readonly" {
   name        = "ReadOnly Role"
   description = "A role that can read Space resources and confirm runs"
-  actions     = ["SPACE_READ", "RUN_TRIGGER", "RUN_CONFIRM", "MODULE_CREATE", "MODULE_ENABLE", "STACK_MANAGE", "MODULE_PUBLISH"]
+  actions     = ["SPACE_READ", "SPACE_WRITE", "RUN_TRIGGER", "RUN_CONFIRM", "MODULE_CREATE", "MODULE_ENABLE", "STACK_MANAGE", "MODULE_PUBLISH"]
 }

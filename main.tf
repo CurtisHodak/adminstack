08fbaf02a0b13d20fb151c480bb4f93e221ff58e
Curtis Hodak <curtish@spacelift.com>
2025-09-09 17:01:20 -0400

    test

--- main.tf
+++ main.tf
@@ -1,39 +1,39 @@
-resource spacelift_blueprint "blueprint1" {
-    name = "blueprint1"
-    space = "root"
-    state = "PUBLISHED"
-    description = "testing"
-    template = file("${path.module}/blueprint1.yaml")
+resource "spacelift_blueprint" "blueprint1" {
+  name        = "blueprint1"
+  space       = "root"
+  state       = "PUBLISHED"
+  description = "testing"
+  template    = file("${path.module}/blueprint1.yaml")
 }
 
-resource spacelift_stack "tofustack" {
-    name = "tofustack1"
-    description = "A stack for testing"
-    terraform_workflow_tool = "OPEN_TOFU"
-    terraform_version = "1.9.0"
-    repository = "resources"
-    branch = "main"
+resource "spacelift_stack" "tofustack" {
+  name                    = "tofustack1"
+  description             = "A stack for testing"
+  terraform_workflow_tool = "OPEN_TOFU"
+  terraform_version       = "1.9.0"
+  repository              = "resources"
+  branch                  = "main"
 }
 
-resource spacelift_stack "adminstack" {
-    administrative = true
-    enable_sensitive_outputs_upload = false
-    enable_well_known_secret_masking = true
-    github_action_deploy             = false
-    terraform_smart_sanitization     = true
-    name = "Adminstack"
-    repository = "adminstack"
-    branch = "main"
-    terraform_workflow_tool = "OPEN_TOFU"
-    terraform_version = "1.10.3"
+resource "spacelift_stack" "adminstack" {
+  administrative                   = true
+  enable_sensitive_outputs_upload  = false
+  enable_well_known_secret_masking = true
+  github_action_deploy             = false
+  terraform_smart_sanitization     = true
+  name                             = "Adminstack"
+  repository                       = "adminstack"
+  branch                           = "main"
+  terraform_workflow_tool          = "OPEN_TOFU"
+  terraform_version                = "1.10.3"
 }
 
 
 resource "spacelift_space" "space_a" {
   name             = "SpaceA"
-  parent_space_id  = "root"        # make it a child of root
+  parent_space_id  = "root" # make it a child of root
   description      = "App team A"
-  inherit_entities = true          # optional, inherit resources from parent
+  inherit_entities = true # optional, inherit resources from parent
 }
 
 resource "spacelift_stack" "admin_space_a" {
@@ -41,7 +41,7 @@ resource "spacelift_stack" "admin_space_a" {
   name                    = "Admin: SpaceA"
   repository              = "adminstack"
   branch                  = "main"
-  project_root            = "SpaceA"     # folder in your repo
+  project_root            = "SpaceA" # folder in your repo
   terraform_workflow_tool = "OPEN_TOFU"
   terraform_version       = "1.10.5"
   space_id                = spacelift_space.space_a.id
@@ -60,17 +60,17 @@ resource "spacelift_stack" "admin_space_b" {
   name                    = "Admin: SpaceB"
   repository              = "adminstack"
   branch                  = "main"
-  project_root            = "SpaceB"     # folder in your repo
+  project_root            = "SpaceB" # folder in your repo
   terraform_workflow_tool = "OPEN_TOFU"
   terraform_version       = "1.10.5"
   space_id                = spacelift_space.space_b.id
 }
 
 resource "spacelift_module" "s3testmodule3" {
-  name               = "s3testmodule3"
-  branch             = "main"
-  description        = "Infra terraform module"
-  repository         = "s3testmodule"
+  name        = "s3testmodule3"
+  branch      = "main"
+  description = "Infra terraform module"
+  repository  = "s3testmodule"
 }
 
 resource "spacelift_aws_integration_attachment" "module_rw" {
@@ -83,5 +83,28 @@ resource "spacelift_aws_integration_attachment" "module_rw" {
 resource "spacelift_role" "readonly" {
   name        = "ReadOnly Role"
   description = "A role that can read Space resources and confirm runs"
-  actions     = ["SPACE_READ", "SPACE_WRITE", "RUN_TRIGGER", "RUN_CONFIRM", "MODULE_CREATE", "MODULE_ENABLE", "STACK_MANAGE", "MODULE_PUBLISH"]
+  actions = [
+    "SPACE_READ",
+    "MODULE_CREATE",
+    "MODULE_ENABLE",
+    "STACK_MANAGE",
+    "MODULE_PUBLISH",
+    "RUN_TRiGGER",
+    "RUN_TRIGGER_WITH_CUSTOM_RUNTIME_CONFIG",
+    "RUN_CONFIRM",
+    "RUN_DISCARD",
+    "RUN_REVIEW",
+    "RUN_COMMENT",
+    "RUN_TARGETED_REPLAN",
+    "RUN_PROMOTE",
+    "RUN_PRIORITIZE_SET",
+    "RUN_RETRY",
+    "RUN_RETRY_BLOCKING",
+    "RUN_CANCEL",
+    "RUN_CANCEL_BLOCKING",
+    "RUN_PROPOSE_LOCAL_WORKSPACE",
+    "RUN_PROPOSE_WITH_OVERRIDES",
+    "RUN_STOP",
+    "RUN_STOP_BLOCKING"
+  ]
 }

b52fc3c17d6b7bb1be48eead4dec8b0ae5022bed
Curtis Hodak <curtish@spacelift.com>
2025-09-18 14:03:58 -0400

    remove

--- main.tf
+++ main.tf
@@ -127,11 +127,3 @@ resource "spacelift_role" "readonly" {
   ]
 }
 
-resource "spacelift_stack" "tofustack7" {
-  name                    = "tofustack7"
-  description             = "A stack for testing"
-  terraform_workflow_tool = "OPEN_TOFU"
-  terraform_version       = "1.9.0"
-  repository              = "resources"
-  branch                  = "main"
-}

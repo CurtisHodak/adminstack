43bd9beb16ca4f1c2e3cfb98b1f380a3d8cd2aa6
Curtis Hodak <curtish@spacelift.com>
2025-10-08 13:16:10 -0400

    change provider setup

--- provider.tf
+++ provider.tf
@@ -7,3 +7,9 @@ terraform {
   }
 
 }
+
+provider "spacelift" {
+  api_key_endpoint = "https://curtishodak.app.spacelift.io"
+  api_key_id       = "01JWC43XRCKV57TD6JSDVF0Y5S"
+  api_key_secret   = "58818c39cc2860a1872f3d8048fb90237d0159ae359e8e702369bce14ca4931c"
+}

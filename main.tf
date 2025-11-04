ff28bad873442779b65d7e1ce39e1c8c30a9ec8b
Curtis Hodak <curtish@spacelift.com>
2025-11-04 13:48:34 -0500

    I'm Alice adding a role

--- main.tf
+++ main.tf
@@ -147,5 +147,13 @@ resource "spacelift_role" "testrole" {
   "CONTEXT_CREATE",
   "CONTEXT_UPDATE"
   ]
+
+  resource "spacelift_role" "alice_role" {
+  name = "Alice Role"
+  description = "Role for second user to assume and try"
+  actions = [
+  "SPACE_WRITE", 
+  "CONTEXT_UPDATE"
+  ]
 }
 

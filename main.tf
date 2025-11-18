c2d5e551b0682714de1c50ae0d33ad5f9498f56b
Curtis Hodak <curtish@spacelift.com>
2025-11-18 14:24:57 -0500

    default policy

--- main.tf
+++ main.tf
@@ -154,7 +154,8 @@ resource "spacelift_role" "alice_role" {
   description = "Role for second user to assume and try"
   actions = [
     "SPACE_WRITE",
-    "CONTEXT_CREATE"
+    "CONTEXT_CREATE",
+    "CONTEXT_UPDATE"
 
   ]
 }

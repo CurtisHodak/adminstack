fcfcf6c3e81951bde471fac3a2a94c7b530af087
Curtis Hodak <curtish@spacelift.com>
2025-11-20 16:48:27 -0500

    add secret

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ resource "spacelift_named_webhook" "test1" {
   # These will be filled in after import
   endpoint = "https://hilaria-nonpelagic-devorah.ngrok-free.dev"
   retry_on_failure = true
+  secret = "my-secret"
 }
 
 import {

f67db45dbf522407d347131990b4b083e827f138
Curtis Hodak <curtish@spacelift.com>
2025-12-04 12:39:48 -0500

    testing ips

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,9 @@ data "spacelift_role_actions" "all" {}
 output "all_role_actions" {
   value = data.spacelift_role_actions.all.actions
 }
+
+data "spacelift_ips" "ips" {}
+
+output "ips" {
+  value = data.spacelift_ips.ips
+}

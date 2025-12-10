18a6b8ec760170d81c1e60660c28828a24aab0a8
Curtis Hodak <curtish@spacelift.com>
2025-12-10 18:09:22 -0500

    testing once without any other things

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,7 @@ output "ips" {
 }
 
 data "spacelift_space_by_path" "spacepath" {
-  space_path = "root/TestSpacebyPath/bypath1/two"
+  space_path = "root/TestSpacebyPath/bypath1/one"
 }
 
 output "space_id" {

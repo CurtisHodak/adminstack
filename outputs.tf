a2ef6daf2bc5d176f38ec07819afc931033d1824
Curtis Hodak <curtish@spacelift.com>
2025-12-11 09:34:57 -0500

    move to root look at enabled our disabled info if it exists in stacks

--- outputs.tf
+++ outputs.tf
@@ -10,16 +10,24 @@ output "ips" {
   value = data.spacelift_ips.ips
 }
 
-data "spacelift_space_by_path" "spacepath" {
-  space_path = "root/TestSpacebyPath/bypath1/two"
-}
+# data "spacelift_space_by_path" "spacepath" {
+#   space_path = "root/TestSpacebyPath/bypath1/two"
+# }
 
-output "space_id" {
-  value       = data.spacelift_space_by_path.spacepath.id
-  description = ""
-}
+# output "space_id" {
+#   value       = data.spacelift_space_by_path.spacepath.id
+#   description = ""
+# }
 
 variable "testing" {
   type    = string
   default = ""
+}
+
+data "spacelift_stacks" "all" {
+
+}
+
+output "all_stacks" {
+  value = data.spacelift_stacks.all.stacks
 }
